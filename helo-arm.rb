4edcd04c0cbe1efc39acbd2457b90d5355930260
Robert Boes <github@robertboes.nl>
2021-12-25 00:57:13 +0100

    Add Helo

---
+++ helo-arm.rb
@@ -0,0 +1,26 @@
+cask "helo-arm" do
+    version "1.6.2"
+    sha256 "b1ef0c2c300d7c644f91cfc664bb28b3066b86257984b0407b52dd0e7ed59d0d"
+  
+    url "https://helo.fra1.digitaloceanspaces.com/helo/HELO-#{version}-arm64.dmg",
+        verified: "helo.fra1.digitaloceanspaces.com"
+    name "HELO"
+    desc "Email tester and debugger"
+    homepage "https://usehelo.com/"
+  
+    livecheck do
+        url "https://usehelo.com/download/latest/mac"
+        strategy :header_match
+    end
+  
+    auto_updates true
+  
+    app "HELO.app"
+  
+    zap trash: [
+        "~/Library/Application Support/HELO",
+        "~/Library/Logs/HELO",
+        "~/Library/Preferences/de.beyondco.helo.plist",
+        "~/Library/Saved Application State/de.beyondco.helo.savedState",
+    ]
+end
